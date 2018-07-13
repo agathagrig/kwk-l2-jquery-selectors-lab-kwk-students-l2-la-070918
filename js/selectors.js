a55aa6482da3493a3fdb82f1117d262834134e07
Agatha Grig <agathagrigorian@gmail.com>
2018-07-13 21:05:29 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,2 +1,19 @@
 'use-strict';
 //add your code here
+function h1Selector() {
+  $("h1")
+}
+
+function liInO1Selector() {
+  
+}
+
+function linkSelector() {
+  
+}
+function imageSelector() {
+  
+}
+function checkbroxInputSelecter() {
+  
+}
