f0abc4f90401490b8e2dcd9fc6827c95b34fdbb5
Agatha Grig <agathagrigorian@gmail.com>
2018-07-13 21:26:12 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -12,7 +12,7 @@ function linkSelector() {
   return $("div#box4.box5 a");
 }
 function imageSelector() {
-  return $("img/cat.jpg");
+  return $("img[alt='catsleeping']");
 }
 function checkbroxInputSelecter() {
   
